3fbf7d13410e00c5540f43a5fa7eb84144acfe66
MooKnee <mooneycatie@gmail.com>
2022-04-21 19:05:50 -0400

    check if this works

--- app/src/main/java/com/example/bookclub/MainActivity.java
+++ app/src/main/java/com/example/bookclub/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bookclub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
