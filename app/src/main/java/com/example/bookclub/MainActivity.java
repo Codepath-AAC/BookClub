85dfff2775fed6e27d008cb23a7251b416ed1390
MooKnee <mooneycatie@gmail.com>
2022-04-22 00:45:36 -0400

    added activities for login, register, profile etc

--- app/src/main/java/com/example/bookclub/MainActivity.java
+++ app/src/main/java/com/example/bookclub/MainActivity.java
@@ -9,9 +9,20 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+    private EditText etBookName;
+    private Button btnBookCover;
+    private ImageView ivPostBook;
+    private Button btnSubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etBookName = findViewById(R.id.etBookName);
+        btnBookCover = findViewById(R.id.btnBookCover);
+        ivPostBook = findViewById(R.id.ivPostBook);
+        btnSubmit = findViewById(R.id.btnSubmit);
     }
 }
