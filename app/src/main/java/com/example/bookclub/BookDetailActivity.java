e67d02f34dfb68923f13c2fdb24faea9d59e66b5
ashleyk82 <ashleykoluda@gmail.com>
2022-05-03 19:20:20 -0400

    where is everyone else

--- app/src/main/java/com/example/bookclub/BookDetailActivity.java
+++ app/src/main/java/com/example/bookclub/BookDetailActivity.java
@@ -57,32 +57,8 @@ public class BookDetailActivity extends AppCompatActivity {
             }
         });
 
-//        binding = ActivityBookDetailBinding.inflate(getLayoutInflater());
-//       setContentView(binding.getRoot());
-//       setSupportActionBar(binding.toolbar);
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_book_detail);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//
-//        binding.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_book_detail);
-//        return NavigationUI.navigateUp(navController, appBarConfiguration)
-//                || super.onSupportNavigateUp();
-
-
 
     }
-
 
 
 }
--- app/src/main/java/com/example/bookclub/HomeScreenActivity.java
+++ app/src/main/java/com/example/bookclub/HomeScreenActivity.java
@@ -14,7 +14,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.bookclub.adapters.BookAdapter1;
+import com.example.bookclub.adapters.BookAdapter2;
 import com.example.bookclub.models.Book1;
+import com.example.bookclub.models.Book2;
 import com.example.bookclub.net.BookClient;
 
 import org.json.JSONArray;
@@ -24,21 +26,19 @@ import java.util.ArrayList;
 
 import okhttp3.Headers;
 
-public class HomeScreenActivity extends AppCompatActivity
-{
+public class HomeScreenActivity extends AppCompatActivity {
 
     public static final String TAG = "HomeScreenActivity";
     Button blogpost, messages, profile, maps, favorites;
     private BookAdapter1 bookAdapter1;
-    //private BookAdapter2 bookAdapter2;
+    private BookAdapter2 bookAdapter2;
     private BookClient client;
     private ArrayList<Book1> book1s;
-    //private ArrayList<Book2> book2s;
+    private ArrayList<Book2> book2s;
     private RecyclerView rvBooks1, rvBooks2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -52,7 +52,7 @@ public class HomeScreenActivity extends AppCompatActivity
         maps = findViewById(R.id.mapsBtn);
         favorites = findViewById(R.id.favoritesBtn);
         book1s = new ArrayList<>();
-        //book2s = new ArrayList<>();
+        book2s = new ArrayList<>();
 
         bookAdapter1 = new BookAdapter1(this, book1s);
 
@@ -82,9 +82,38 @@ public class HomeScreenActivity extends AppCompatActivity
 
         fetchBooks("Trending");
 
-        blogpost.setOnClickListener(new View.OnClickListener(){
+        bookAdapter2 = new BookAdapter2(this, book2s);
+
+        bookAdapter2.setOnItemClickListener(new BookAdapter2.OnItemClickListener() {
             @Override
-            public void onClick(View view){
+            public void onItemClick(View itemView, int position) {
+                Toast.makeText(
+                        HomeScreenActivity.this,
+                        "An item at position " + position + " clicked!",
+                        Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(HomeScreenActivity.this, BookDetailActivity.class);
+                Book2 book2 = book2s.get(position);
+                String title = book2.getTitle();
+                String auth = book2.getAuthor();
+                String image = book2.getCoverUrl();
+                intent.putExtra("title", title);
+                intent.putExtra("author", auth);
+                intent.putExtra("image", image);
+                startActivity(intent);
+            }
+        });
+        rvBooks2.setAdapter(bookAdapter2);
+        LinearLayoutManager llm2 = new LinearLayoutManager(this);
+        llm2.setOrientation(LinearLayoutManager.HORIZONTAL);
+        rvBooks2.setLayoutManager(llm2);
+
+        fetchBooks2("Oscar Wilde");
+
+
+        blogpost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent i = new Intent(HomeScreenActivity.this, BlogpostActivity.class);
                 startActivity(i);
             }
@@ -160,41 +189,41 @@ public class HomeScreenActivity extends AppCompatActivity
         });
     }
 
-//    private void fetchBooks2(String query) {
-//        client = new BookClient();
-//        client.getBooks(query, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Headers headers, JSON response) {
-//                try {
-//                    JSONArray docs;
-//                    if (response != null) {
-//                        // Get the docs json array
-//                        docs = response.jsonObject.getJSONArray("docs");
-//                        // Parse json array into array of model objects
-//                        final ArrayList<Book2> books = Book2.fromJson(docs);
-//                        // Remove all books from the adapter
-//                        book2s.clear();
-//                        // Load model objects into the adapter
-//                        for (Book2 book : books) {
-//                            book2s.add(book); // add book through the adapter
-//                        }
-//                        bookAdapter2.notifyDataSetChanged();
-//                    }
-//                } catch (JSONException e) {
-//                    // Invalid JSON format, show appropriate error.
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Headers headers, String responseString, Throwable throwable) {
-//                // Handle failed request here
-//                Log.e(HomeScreenActivity.class.getSimpleName(),
-//                        "Request failed with code " + statusCode + ". Response message: " + responseString);
-//            }
-//        });
+    private void fetchBooks2(String query) {
+        client = new BookClient();
+        client.getBooks(query, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON response) {
+                try {
+                    JSONArray docs;
+                    if (response != null) {
+                        // Get the docs json array
+                        docs = response.jsonObject.getJSONArray("docs");
+                        // Parse json array into array of model objects
+                        final ArrayList<Book2> books = Book2.fromJson(docs);
+                        // Remove all books from the adapter
+                        book2s.clear();
+                        // Load model objects into the adapter
+                        for (Book2 book : books) {
+                            book2s.add(book); // add book through the adapter
+                        }
+                        bookAdapter2.notifyDataSetChanged();
+                    }
+                } catch (JSONException e) {
+                    // Invalid JSON format, show appropriate error.
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String responseString, Throwable throwable) {
+                // Handle failed request here
+                Log.e(HomeScreenActivity.class.getSimpleName(),
+                        "Request failed with code " + statusCode + ". Response message: " + responseString);
+            }
+        });
 
 
-
+    }
 }
